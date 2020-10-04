62f814b9648ea3b7eac68f3a5799de087bc724be
Manczak Adrian <manczakadrian@gmail.com>
2020-10-04 03:36:10 +0200

    Initial commit

---
+++ src/test/java/pl/manczak/springbootmapinit/SpringbootMapInitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manczak.springbootmapinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
